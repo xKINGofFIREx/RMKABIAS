7ffc79af624ee8b43890cd160333d6ee38c60563
xKINGofFIREx <artemilin270703@gmail.com>
2025-11-03 11:53:41 +0300

    practice 2

---
+++ Lesson2/intentfilter/src/main/java/com/mirea/ilinav/intentfilter/MainActivity.java
@@ -0,0 +1,50 @@
+package com.mirea.ilinav.intentfilter;
+
+
+import static android.content.ContentValues.TAG;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    private Button btnS;
+    private Button btnFIO;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        btnS = findViewById(R.id.btnS);
+        btnFIO = findViewById(R.id.btnFIO);
+
+        btnS.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG,"ClickedSITE");
+                Uri address = Uri.parse("https://www.mirea.ru/");
+                Intent openLinkIntent = new Intent(Intent.ACTION_VIEW, address);
+                startActivity(openLinkIntent);
+            }
+        });
+
+        btnFIO.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG,"ClickedFIO");
+                Intent shareIntent = new Intent(Intent.ACTION_SEND);
+                shareIntent.setType("text/plain");
+                shareIntent.putExtra(Intent.EXTRA_SUBJECT, "MIREA");
+                shareIntent.putExtra(Intent.EXTRA_TEXT, "Тишков Григорий Валерьевич");
+                startActivity(Intent.createChooser(shareIntent, "Тишков Г.В."));
+            }
+        });
+    }
+}
